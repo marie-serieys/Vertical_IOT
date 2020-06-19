67d47c74961eb18978857d09bb6431be7648a3e9
Baptiste Pelloux <pelloux.baptiste@gmail.com>
2020-06-19 16:46:25 +0200

    Modification du main

--- main.tf
+++ main.tf
@@ -1,18 +1,16 @@
-provider « scaleway » {
-region = « par1 »
-access_key = « ${var.secret_access_key} »
-secret_key = « ${var.secret_key)
+provider "scaleway" {
+  region     = "par1"
+  access_key = "${var.secret_access_key}"
+  secret_key = "${var.secret_key}"
 }
-data « scaleway_image » « chirpstack »{
-architecture =
-name = « Ubuntu 20.04 Focal Fossa »
+data "scaleway_image" "chirpstack" {
+  name = "Ubuntu 20.04 Focal Fossa"
 }
 
-ressource « scaleway_server » « chirp stack »{
-name = « chirp stack »
-image =
-type = « DEV1-S »
-state = « stopped »
-enable_ipv6 = true
+ressource "scaleway_server" "chirpstack" {
+  name        = "chirpstack"
+  type        = "DEV1-S"
+  state       = "stopped"
+  enable_ipv6 = true
 
 }
